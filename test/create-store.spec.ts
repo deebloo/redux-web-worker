87308f921ca21a8a4c399609ce386fd16fc53ecb
Danny Blue <dannybluedesign@gmail.com>
2016-07-02 23:24:53 -0400

    add unit tests

--- test/create-store.spec.ts
+++ test/create-store.spec.ts
@@ -1,21 +1,17 @@
 import { createStore } from '../src';
 
-console.log(createStore);
-
 describe('Create Store', () => {
   it('should create a store', () => {
-    // var store = createStore(function (state, action) {
-    //   return action.state;
-    // });
+    var store = createStore(function (state, action) {
+      return action.state;
+    });
 
-    // store.subscribe(function (state) {
-    //   expect(state).toBe('Hello World');
-    // });
+    store.subscribe(function (state) {
+      expect(state).toBe('Hello World');
+    });
 
-    // store.dispatch({
-    //   state: 'Hello World'
-    // });
-
-    expect('Hello World').toBe('Hello World');
+    store.dispatch({
+      state: 'Hello World'
+    });
   })
 });
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './src',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ }
+    ]
+  }
+}
