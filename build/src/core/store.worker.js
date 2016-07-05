7f9058777e751e4003a64580157206eb13389091
Danny Blue <dannybluedesign@gmail.com>
2016-07-05 08:20:37 -0400

    update build

--- build/src/core/store.worker.js
+++ build/src/core/store.worker.js
@@ -6,7 +6,7 @@ function createWorker(fn, initialState) {
     // store default state
     // manages get state
     // assigns reducer
-    var blob = new Blob([("\n    self.state = " + JSON.stringify(initialState) + ";\n\n    self.reducer = " + fn.toString() + ";\n\n    self.onmessage = function (e) {\n      if (e.data.type !== '" + config_1.actions.GET_STATE + "') {\n        self.state = self.reducer(self.state, e.data);\n      }\n\n      self.postMessage(self.state);\n    }\n  ")], { type: 'text/javascript' });
+    var blob = new Blob([("\n    self.state = " + JSON.stringify(initialState) + ";\n\n    self.reducer = " + fn.toString() + ";\n\n    self.onmessage = function (e) {\n      if (e.data.type !== " + JSON.stringify(config_1.actions.GET_STATE) + ") {\n        self.state = self.reducer(self.state, e.data);\n      }\n\n      self.postMessage(self.state);\n    }\n  ")], { type: 'text/javascript' });
     var url = URL.createObjectURL(blob);
     var worker = new Worker(url);
     URL.revokeObjectURL(url);
