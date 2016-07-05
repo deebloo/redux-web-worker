65ea0d4432f283843e770316e745fae78003b8e1
Danny Blue <dannybluedesign@gmail.com>
2016-07-05 11:35:51 -0400

    update tests

--- test/create-store.spec.ts
+++ test/create-store.spec.ts
@@ -1,9 +1,28 @@
 import { createStore } from '../src';
 
 describe('Create Store', () => {
-  it('should create a store', done => {
+  it('should create a store and have an initial state', done => {
     var store = createStore((state, action) => {
-      switch(action.type) {
+      switch (action.type) {
+        case 'INCREMENT':
+          return state + 1;
+        case 'DECREMENT':
+          return state - 1;
+        default:
+          return state;
+      }
+    }, 0);
+
+    store.getState(state => {
+      expect(state).toBe(0);
+
+      done();
+    });
+  });
+
+  it('should increment state by 1', done => {
+    var store = createStore((state, action) => {
+      switch (action.type) {
         case 'INCREMENT':
           return state + 1;
         case 'DECREMENT':
@@ -19,27 +38,15 @@ describe('Create Store', () => {
       done();
     });
 
-    store.dispatch({ type: 'INCREMENT' });
-  });
-
-  it('should get the current state', done => {
-    var store = createStore((state, action) => {
-      return state;
-    }, 'Hello World');
-
-    store.getState(function (state) {
-      expect(state).toBe('Hello World');
-
-      done();
-    });
+    store.dispatch({ type: 'INCREMENT' })
   });
 
   it('should unsubscribe from store', () => {
     var store = createStore((state, action) => {
       return state;
-    }, 'Hello World');
+    });
 
-    var subscription = store.subscribe(state => {});
+    var subscription = store.subscribe(state => { });
 
     subscription.unsubScribe();
   });
